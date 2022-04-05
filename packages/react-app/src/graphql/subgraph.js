2a4f09319ac62372c058628562c47089e41adc7a
julienbrg <julien@strat.cc>
2022-04-05 23:01:53 +0200

    add subgraph

--- packages/react-app/src/graphql/subgraph.js
+++ packages/react-app/src/graphql/subgraph.js
@@ -1,15 +1,15 @@
 import { gql } from "@apollo/client";
 
-// See more example queries on https://thegraph.com/explorer/subgraph/paulrberg/create-eth-app
 const GET_TRANSFERS = gql`
-  {
-    transfers(first: 10) {
+{
+  users {
+    id
+    tokens {
       id
-      from
-      to
-      value
+      contentURI
     }
   }
+}
 `;
 
 export default GET_TRANSFERS;
--- packages/react-app/src/index.tsx
+++ packages/react-app/src/index.tsx
@@ -16,9 +16,6 @@ const config = {
   },
 }
 
-// You should replace this url with your own and put it into a .env file
-// See all subgraphs: https://thegraph.com/explorer/
-
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
